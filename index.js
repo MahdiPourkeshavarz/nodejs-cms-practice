2539d517307da10f0922227939d7d2e9c59d8966
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2025-03-23 16:19:19 +0330

    feat(home route): outsourcing api calls for home route and building components for it

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require("path");
 const expressHandlebars = require("express-handlebars");
 const app = express();
 
+const mainRoutes = require("./routes/home/main.js");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -11,9 +13,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.engine("handlebars", expressHandlebars({ defaultLayout: "home" }));
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res) => {
-  res.render("home/index");
-});
+app.use("/", mainRoutes);
 
 app.listen(3008, () => {
   console.log("listening...");
