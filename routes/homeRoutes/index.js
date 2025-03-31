4d45e3d5520854f9569ca6732807bdab8cc8a3da
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2025-03-31 18:35:27 +0330

    feat(home page) : rendering posts in the home page

--- routes/homeRoutes/index.js
+++ routes/homeRoutes/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const router = express.Router();
-
+const Post = require("../../models/Post");
 router.all("/*", (req, res, next) => {
   req.app.locals.layout = "home";
   next();
 });
 
-router.get("/", (req, res) => {
-  res.render("home/index");
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.render("home/index", { posts });
+  } catch (error) {
+    res.status(404).send("no post to display");
+  }
 });
 
 router.get("/about", (req, res) => {
